ca8da269f7cbe0ef0ad939a23933d06e14c1c6c3
jamal-uddin-asif <asifzehendmg@gmail.com>
2025-11-12 01:42:28 +0600

    Feature of this web secton added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,9 @@ import MyContainer from "../../Components/MyContainer/MyContainer";
 import { useAxiosSecure } from "../../Hooks/useAxiosSecure";
 import LatestBooksCard from "../../Components/Home/LatestBooksCard";
 import { BiSolidBookAdd } from "react-icons/bi";
+import { FaBookOpenReader } from "react-icons/fa6";
+import { MdAutoDelete } from "react-icons/md";
+import { TiEdit } from "react-icons/ti";
 
 const Home = () => {
   const axiosSecure = useAxiosSecure();
@@ -56,6 +59,16 @@ const Home = () => {
               >
                 Create Book
               </Link>
+            </div>
+          </div>
+
+          <div className="">
+            <h1 className="border-b mb-4 my-heading py-4 text-2xl">Feature of this web</h1>
+            <div className="sm:flex space-y-4 text-center justify-center gap-7 py-6">
+              <div className="bg-linear-to-br from-green-400 to-green-800 p-6 rounded-2xl text-"><div className="flex justify-center"><FaBookOpenReader size={30}/></div>Read book</div>
+              <div className="bg-linear-to-br from-orange-400 to-orange-700 p-6 rounded-2xl text-"><div className="flex justify-center"><BiSolidBookAdd size={30}/></div>Add book</div>
+              <div className="bg-linear-to-br from-yellow-300 to-yellow-600 p-6 rounded-2xl text-"><div className="flex justify-center"><TiEdit size={30} /></div>Edit book</div>
+              <div className="bg-linear-to-br from-red-500 to-red-800 p-6 rounded-2xl text-"><div className="flex justify-center"><MdAutoDelete size={30} /></div>Delete book</div>
             </div>
           </div>
 
